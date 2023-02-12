97c93291e05b03f3a037bfa2fb5e7d3b8148939a
estud <estudanteakihiko@gmail.com>
2023-02-12 01:13:40 -0300

    primeiro commit e push

---
+++ src/exercicios/ConverterDolarParaReal.java
@@ -0,0 +1,30 @@
+package exercicios;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ConverterDolarParaReal {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        Locale local = new Locale("en", "US");
+        NumberFormat formataValores = NumberFormat.getCurrencyInstance(local);
+
+        System.out.print("Digite o valor da cotação do dólar: ");
+        BigDecimal cotacaoDolar =  new BigDecimal(leitor.nextLine());
+
+        System.out.print("Digite o valor em reais: ");
+        BigDecimal real =  new BigDecimal(leitor.nextLine());
+
+        BigDecimal dolar = real.divide(cotacaoDolar, 4, RoundingMode.HALF_EVEN);;
+
+        System.out.println("O valor em dólares é: " + formataValores.format(dolar));
+
+    }
+}
+// Obs: o dolar no Google Finanças esta com o valor = 5.2151
+// https://www.google.com/finance/quote/USD-BRL?sa=X&ved=2ahUKEwiFp7X2gY_9AhXrrJUCHakYAPIQmY0JegQIBhAd
+// Percebi que quando fazemos coversão a partir de 1500 reais, o valor em dolar começa a ficar diferente quando comparado com o valor do google.
+// Acredito que seja necessario uma maior precisão do valor da cotação do dolar, mas não tenho certeza
