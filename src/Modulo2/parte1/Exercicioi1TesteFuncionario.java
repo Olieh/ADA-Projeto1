c5643a19296dd4699885d45b81365a02a722ea8b
estud <estudanteakihiko@gmail.com>
2023-03-03 17:51:51 -0300

    comitando e enviando

--- src/Modulo2/parte1/Exercicioi1TesteFuncionario.java
+++ src/Modulo2/parte1/Exercicioi1TesteFuncionario.java
@@ -3,11 +3,7 @@ package Modulo2.parte1;
 public class Exercicioi1TesteFuncionario {
     public static void main(String[] args) {
         Funcionario funcionario1 =  new Funcionario();
-        funcionario1.matricula = 10992;
-        funcionario1.nome = "João";
-        funcionario1.cargo = "PROGRAMADOR";
-        funcionario1.salarioHora = 15 ;
-        funcionario1.quantidadeHora = 160;
+        funcionario1.entradaDados();
         funcionario1.mostrarDados();
     }
 }
--- src/Modulo2/parte1/Funcionario.java
+++ src/Modulo2/parte1/Funcionario.java
@@ -1,11 +1,27 @@
 package Modulo2.parte1;
 
+import java.util.Scanner;
+
 public class Funcionario {
     public int matricula;
     public String nome;
     public String cargo;
     public double salarioHora;
     public double quantidadeHora;
+
+    public void entradaDados(){
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Informe a matricula do funcionario");
+        matricula = Integer.parseInt(leitor.nextLine());
+        System.out.println("Informe o nome do funcionario");
+        nome = leitor.nextLine();
+        System.out.println("Informe o cargo do funcionario");
+        cargo = leitor.nextLine();
+        System.out.println("Informe o salario hora do funcionario");
+        salarioHora = Integer.parseInt(leitor.nextLine());
+        System.out.println("Informe a quantidade de hora trabalhada do funcionario");
+        quantidadeHora = Integer.parseInt(leitor.nextLine());
+    }
 
     public void mostrarDados(){
         System.out.println("Matricula: " + matricula);
